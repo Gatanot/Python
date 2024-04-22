51bc023206a957aef22d80ebd4f67d11babe1963
Gatanot <nesap123@qq.com>
2024-04-22 22:14:53 +0800

    24.04.22.01

---
+++ PythonLearning/chap02.py
@@ -0,0 +1,10 @@
+import time
+currenttime=time.time();#获取当前时间
+print(currenttime)
+totalseconds=int(currenttime)
+currentseconds=totalseconds%60
+totalminutes=totalseconds//60#得到整数的除法
+currentminutes=totalminutes%60
+totalhours=totalminutes//60
+currenthours=totalhours%24
+print("current time is: ",currenthours,":",currentminutes,":",currentseconds)
