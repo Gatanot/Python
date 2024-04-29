ff6035dd328204525552d1f8cf4fe259dfcac54a
Gatanot <nesap123@qq.com>
2024-04-29 18:11:46 +0800

    24.04.29.02

--- PythonLearning/chap03 数学函数,字符串,对象.py
+++ PythonLearning/chap03 数学函数,字符串,对象.py
@@ -1,68 +1,69 @@
-#3.2 常见的Python函数
-x=2
+# 3.2 常见的Python函数
+x = 2
 print(abs(x))
-x1=4
-x2=2
-print(max(x1,x2))
-print(min(x1,x2))
-print(pow(x1,x2))
+x1 = 4
+x2 = 2
+print(max(x1, x2))
+print(min(x1, x2))
+print(pow(x1, x2))
 print(round(3.5))
-print(round(3.312,2))
+print(round(3.312, 2))
 import math
-x=1.0
+
+x = 1.0
 print(math.fabs(x))
 print(math.ceil(x))
-#向上取整，不改变值
+# 向上取整，不改变值
 print(math.floor(x))
-#向下取整
+# 向下取整
 print(math.exp(x))
-#e^x
+# e^x
 print(math.log(x))
-#log_e x
+# log_e x
 print(math.sqrt(x))
-#x^(1/2)
+# x^(1/2)
 print(math.sin(x))
 print(math.asin(x))
-#arcsin(x)
+# arcsin(x)
 print(math.cos(x))
 print(math.acos(x))
-#arccos(x)
+# arccos(x)
 print(math.tan(x))
 print(math.degrees(x))
-#弧度转角度
+# 弧度转角度
 print(math.radians(x))
-#角度转弧度
-#3.3 字符串和字符
-ch='A'
-code=ord(ch)
-#ASCII码
-ch=chr(code)
-#对应字符
-print("Hello World",end='')
-#不换行打印
-string=str(12342)
-#数字转字符串
-#3.5 对象和方法
-#python中所有的数据都是对象
-n=3
+# 角度转弧度
+# 3.3 字符串和字符
+ch = "A"
+code = ord(ch)
+# ASCII码
+ch = chr(code)
+# 对应字符
+print("Hello World", end="")
+# 不换行打印
+string = str(12342)
+# 数字转字符串
+# 3.5 对象和方法
+# python中所有的数据都是对象
+n = 3
 id(n)
 type(n)
 string.lower()
-#转小写
+# 转小写
 string.upper()
-#转大写
-#3.6 格式化数字和字符串
-interest=123.4342
-print("the number is",format(interest,"10.2f"))
-#宽度为10，小数点后为2为的小鼠
-print("the number is",format(interest,"10.2e"))
-#科学计数法
-print("the number is",format(interest,"10.2%"))
-#百分数输出
-print("the number is",format(interest,"<10.2f"))
-#默认为右对齐，加<改为左对齐
-#d,x,o,b格式化十进制，十六进制，八进制，二进制整数
-print(format("dfafiufhsiufhisufui","10s"))
-#格式化宽度为20的字符串
-#3.7 绘制图形
-#3.8 绘制带颜色和字体的图形
+# 转大写
+# 3.6 格式化数字和字符串
+interest = 123.4342
+print("the number is", format(interest, "10.2f"))
+# 宽度为10，小数点后为2为的小鼠
+print("the number is", format(interest, "10.2e"))
+# 科学计数法
+print("the number is", format(interest, "10.2%"))
+# 百分数输出
+print("the number is", format(interest, "<10.2f"))
+# 默认为右对齐，加<改为左对齐
+# d,x,o,b格式化十进制，十六进制，八进制，二进制整数
+print(format("dfafiufhsiufhisufui", "10s"))
+# 格式化宽度为20的字符串
+# 3.7 绘制图形
+# 3.8 绘制带颜色和字体的图形
--- PythonLearning/chap04 选择.py
+++ PythonLearning/chap04 选择.py
@@ -1,27 +1,28 @@
-#4.2 布尔类型、数值和表达式
-print(int(1<0))
-print(bool(1<0))
-#4.3 产生随机数字
+# 4.2 布尔类型、数值和表达式
+print(int(1 < 0))
+print(bool(1 < 0))
+# 4.3 产生随机数字
 import random
-a=1
-b=9
-print(random.randint(a,b))#产生一个a,b间的随机整数,包括b
-print(random.randrange(a,b))#同上,不包括
-print(random.uniform(a,b))#产生一个a,b间的随机浮点数
-#4.4 if语句
-x = random.randint(a,b)
-if x>(a+b)/2 :
+
+a = 1
+b = 9
+print(random.randint(a, b))  # 产生一个a,b间的随机整数,包括b
+print(random.randrange(a, b))  # 同上,不包括
+print(random.uniform(a, b))  # 产生一个a,b间的随机浮点数
+# 4.4 if语句
+x = random.randint(a, b)
+if x > (a + b) / 2:
     print(x)
-#4.6 if-else语句
-if x>(a+b)/2:
+# 4.6 if-else语句
+if x > (a + b) / 2:
     print(x)
 else:
-    print((a+b)/2)
-#4.7 嵌套if与多重if-else语句
-#4.11 逻辑运算符
+    print((a + b) / 2)
+# 4.7 嵌套if与多重if-else语句
+# 4.11 逻辑运算符
 print(not (True and False or True))
-#4.14 条件表达式
-print(x if x<5 else 5)
-#expression1 if expression2 else expression3
-#expression2为真则值为expression1，否则为expression3
-#4.15 运算符的优先级与结合方向
+# 4.14 条件表达式
+print(x if x < 5 else 5)
+# expression1 if expression2 else expression3
+# expression2为真则值为expression1，否则为expression3
+# 4.15 运算符的优先级与结合方向
